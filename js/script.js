3569d03427cd0daa89028b22bb97c2593d640f83
Nicole <nicole.e.bryson@gmail.com>
2020-07-24 15:26:27 -0400

    fixed menu things and jquery issues

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle'){
     x.className += ' responsive';
+    $(".navtoggle.responsive nav").fadeIn(1000);
   } else {
     x.className = 'navtoggle';
+    $(".navtoggle nav").attr({style: ''});
   }
 }
